9e786b9541be48f13241117cad62385586cd0d98
sansk <iamsangyk@gmail.com>
2022-12-13 08:07:53 +0530

    Day 07 - Initial Commit

---
+++ Day07/index.js
@@ -0,0 +1,10 @@
+import { getInput, log } from '../utils/index.js';
+import chalk from 'chalk';
+
+const input = getInput(import.meta.url).replace(/\r/g, "").trim();
+const p1 = chalk.bgMagenta,
+    p2 = chalk.bgGreen,
+    t1 = chalk.magenta,
+    t2 = chalk.green;
+
+    
